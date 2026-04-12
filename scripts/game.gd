90f025332c3f0c9c8e31e65690574213ed40f92c
Hunter Blanks <hblanks@gmail.com>
2026-04-11 23:33:44 -0500

    Added arguments to spawn_racer calls to spawn 3 different colored racers on the screen

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,13 +4,12 @@ var racer_scene = preload("res://scenes/racer.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn_racer()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	spawn_racer(Racer.DotColor.RED, Vector2(100, 300))
+	spawn_racer(Racer.DotColor.GREEN, Vector2(200, 300))
+	spawn_racer(Racer.DotColor.BLUE, Vector2(300, 300))
 
 func spawn_racer(color, position):
 	var racer = racer_scene.instantiate()
+	racer.racer_color = color
+	racer.position = position
 	add_child(racer)
